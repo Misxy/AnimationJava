f4341d5c6c960a7cdd130d76a4e44acc3b83b94f
Supakorn Chupraphawan <Xaster74@gmail.com>
2016-11-26 01:14:58 +0700

    11/26/2016 1.15.AM

--- app/src/main/java/com/example/john/project1/Encap_Animation.java
+++ app/src/main/java/com/example/john/project1/Encap_Animation.java
@@ -31,11 +31,11 @@ public class Encap_Animation extends AppCompatActivity {
         //ImageView
         pointer = (ImageView)findViewById(R.id.pointer_datahide);
         code_encap = (ImageView)findViewById(R.id.code_encap);
-        class_encap = (ImageView)findViewById(R.id.class_encap);
+        class_encap = (ImageView)findViewById(R.id.class_DH);
 
         //TextView
-        objEncap_text = (TextView)findViewById(R.id.object_encap_text);
-        classEncap_text = (TextView)findViewById(R.id.classEncap_text);
+        objEncap_text = (TextView)findViewById(R.id.object_DH_text);
+        classEncap_text = (TextView)findViewById(R.id.classDH_text);
         getter_text =(TextView)findViewById(R.id.getter_text);
         setter_text =(TextView)findViewById(R.id.setter_text);
         Getter_box = (TextView)findViewById(R.id.GetterName_box);
@@ -44,7 +44,7 @@ public class Encap_Animation extends AppCompatActivity {
         output_result = (TextView)findViewById(R.id.output_reseult);
         text_parameter = (TextView)findViewById(R.id.text_parameter);
         //Linearlayout
-        output_layout = (LinearLayout)findViewById(R.id.output_layout);
+        output_layout = (LinearLayout)findViewById(R.id.output_layoutDH);
 
         //ImageButtons
         PlaybtnEncap = (ImageButton)findViewById(R.id.playbtnEncap);
--- app/src/main/java/com/example/john/project1/dh_animation.java
+++ app/src/main/java/com/example/john/project1/dh_animation.java
@@ -1,13 +1,28 @@
 package com.example.john.project1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class dh_animation extends AppCompatActivity {
+    ImageView pointer;
+    ImageButton playbtn,pausebtn,stopbtn;
+    ScrollView sv_codeDH;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dh_animation);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(this,List_chapter.class);
+        startActivity(intent);
+        finish();
     }
 }
